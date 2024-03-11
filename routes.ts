7741311e3d0f2c32dbeb18f29fc1534f77412a64
Gabriel Lima <gabriel.lbs002@gmail.com>
2024-03-11 13:07:11 -0300

    feat: prisma setup, register/login/logout

---
+++ routes.ts
@@ -0,0 +1,30 @@
+/**
+ * An array of routes that are accessible to the public
+ * 
+ * These routes do not require authentication
+ */
+export const publicRoutes = [
+  '/'
+]
+
+/**
+ * An array of routes that are used for authentication
+ * 
+ * These routes will redirect logged in users to /settings
+ */
+export const authRoutes = [
+  '/auth/login',
+  '/auth/register'
+]
+
+/**
+ * The prefix for API authentication routes
+ * 
+ * Routes with this prefix are used for API authentication purposes
+ */
+export const apiAuthPrefix = '/api/auth'
+
+/**
+ * The default redirect path after logging in
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings'
